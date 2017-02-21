4060a8b5210a9a7c511610da86361bbea6c95071
Lena <lenadroid@yandex.com>
2017-02-20 22:26:53 -0800

    some work in progress for snapshot node

--- src/ChandyLamportFSharp/SnapshotNode.fsx
+++ src/ChandyLamportFSharp/SnapshotNode.fsx
@@ -10,20 +10,203 @@ open NetworkingNode
 open Newtonsoft.Json
 open System.Text
 open Communication
+open System.Collections.Generic
 
 (*
-    Next goals:
-    - Adding neighbors to the collection
-    - Heartbeat mechanism to keep active neighbors collection up to date
-    - Unique identifiers for new messages
-    - Log and history of all messages
+    Termination:
+        A node who initiated snapshot knows that it's finished when it has received marker messages through all of its incoming channels.
+
+        To save a state of a snapshot:
+            1. Each node will have its state saved on disk
+            2. There will be a global Snapshot instance that has the state of all the nodes as soon as it's available
+
+    Each node needs to have fields:
+    	- State value
+        - State snapshot (which will also indicate if this node has already received a marker message)
+        - Messages to snapshot (after a node has already done state snapshot, it needs to persist all the messages in transit as channel state)
+        - Incoming channels snapshot
+        - Tracking which incoming channels have already sent their back-markers to us
+        -
+
 *)
+
+/// This type indicates what the state of nodes would look like.
+type Contents = int
+
+/// Message is an alias for a basic message, sending certain content type.
+type Message = BasicMessage<Contents>
+
+/// Channel state is a list of messages in transit from one node to another one.
+type ChannelState = ResizeArray<Message>
+
+/// Neighbor is a type for naming connected nodes and combining it with their endpoints.
+type Neighbor = {
+    name: string
+    endpoint: ServerEndpoint
+}
+
+type ShouldITakeASnapshot = Yes | AlreadyDone
 
 /// A node of bunnywolf distributed system.
 /// When an instance is created - a TCP server is started listening for requests on specified ipAddress and port.
 /// Nodes can exchange messages with each other, keep track of neighbors and be a part of a distributed system.
+/// Snapshot nodes can initiate a snapshot
 type SnapshotNode(id, ipAddress, port) =
-    member this.n = new NetworkingNode(id, ipAddress, port, this.MessageRoundAbout)
+    let mutable state = 0
+    let mutable stateSnapshot = 0
+    let mutable shouldTakeASnapshot = Yes
+
+    member this.N = new NetworkingNode(id, ipAddress, port, this.MessageRoundAbout)
+
+    // Indicates whether a node needs to take a local snapshot
+    member this.ShouldTakeASnapshot
+        with get () = shouldTakeASnapshot
+        and set (value) = shouldTakeASnapshot <- value
+
+    /// Current state of current node.
+    member this.State
+        with get () = state
+        and set value = state <- value
+
+    /// Snapshotted state of current node.
+    /// It should persisted on disk
+    member this.StateSnapshot
+        with get () = stateSnapshot
+        and set value = stateSnapshot <- value
+
+    // Represents all the nodes current node can reach.
+    member val ConnectedNeighbors = new ResizeArray<ServerEndpoint>()
+
+    // Represents a map of nodes and states of channels going from indicated server endpoints to current node.
+    member val ChannelStatesSnapshot = new Dictionary<ServerEndpoint, ChannelState> ()
+
+    // Messages received by the current node for which it has already sent an acknowledgement to message sender.
+    member val AcknowledgedReceivedMessages = new ResizeArray<Message>()
+
+    // Messages sent by the current node but not yet acknowledged delivery to its destination.
+    member val SentButUnacknowledgedMessages = new ResizeArray<Message>()
+
+    (* State operations *)
+
+    member this.HasEnoughToTransfer amount = async {
+        return not <| (this.State - amount < 0)
+    }
+
+    (* Neighbors *)
+
+    member this.AddNeighbor (endpoint: ServerEndpoint) = async {
+        try
+            if not <| this.ConnectedNeighbors.Contains(endpoint)
+            then this.ConnectedNeighbors.Add(endpoint)
+        with
+        | e -> printf "Exception adding outgoing channel to node %A:%A %A" endpoint.Ip endpoint.Port e
+    }
+
+    (* Snapshots *)
+
+    member this.InitiateSnapshot () = async {
+        // Capture own state into StateSnapshot
+        // Send snapshot messages to all connected neighbors
+        //
+        ()
+    }
+
+    member this.ReceiveSnapshotMessage () = async {
+        ()
+    }
+
+    member this.SendSnapshotMessage () = async {
+        ()
+    }
+
+    member this.ComputeAndPersistChannelState () = async {
+        // Compare list of messages sender node sent to us and haven't received an ACK for
+        // and a list of messages from sender we have sent an ACK for, and finding the difference.
+        // The difference is going to be messages in transit for snapshot.
+        ()
+    }
+
+    member this.ComputeAndPersistNodeState () = async {
+        // Save a value from state into
+        ()
+    }
+
+    member this.TerminateSnapshotForMyself () = async {
+        ()
+    }
+
+
+    (* Basic messages *)
+
+(*
+	SendMoney(p, q) ==
+	    /\ RuleApplicationCount' = RuleApplicationCount + 1
+	    /\ \E m \in 1 .. NodeStates[p] : 
+	        /\ MessagesInChannels' = [MessagesInChannels EXCEPT ![p] = [MessagesInChannels[p] EXCEPT ![q] = Append(MessagesInChannels[p][q], m)]]
+	        /\ NodeStates' = [NodeStates EXCEPT ![p] = NodeStates[p] - m]
+	    /\ UNCHANGED<<MessagesToSnapshot, MarkersSent, NodeStateSnapshots, ChannelStateSnapshots>>
+	 
+	ReceiveMoney(p, q) ==
+	    /\ RuleApplicationCount' = RuleApplicationCount + 1
+	    /\ MessagesInChannels[p][q] # <<>>
+	    /\ Head(MessagesInChannels[p][q]) # -1
+	    /\ NodeStates' = [NodeStates EXCEPT ![q] = NodeStates[q] + Head(MessagesInChannels[p][q])]
+	    /\ MessagesInChannels' = [MessagesInChannels EXCEPT ![p] = [MessagesInChannels[p] EXCEPT ![q] = Tail(MessagesInChannels[p][q])]]
+	    /\ MessagesToSnapshot' =
+	           IF NodeStateSnapshots[q] < 0
+	           THEN MessagesToSnapshot
+	           ELSE [MessagesToSnapshot EXCEPT ![p] = [MessagesToSnapshot[p] EXCEPT ![q] = Append(MessagesToSnapshot[p][q], Head(MessagesInChannels[p][q]))]]
+	    /\ UNCHANGED<<MarkersSent, NodeStateSnapshots, ChannelStateSnapshots>>
+*)
+
+    member this.SendBasicMessage (amount: Contents) (node: ServerEndpoint) = async {
+        let! invariant = this.HasEnoughToTransfer amount
+        if invariant then
+            // Extracting the amount from current state.
+            this.State <- this.State - amount
+            // Preparing a message with amount to send.
+            let messageToSend =
+                { 
+                    id = Guid.NewGuid().ToString()
+                    contents = amount
+                    address = ipAddress.ToString()
+                    port = port
+                    needAck = true
+                }
+            // Putting the message in the list of messages sent, but not yet acknowledged.
+            this.SentButUnacknowledgedMessages.Add messageToSend
+            // Sending a basic message to known endpoint.
+            do! this.N.SendMessageToNeighbor node messageToSend
+        else ()
+    }
+
+    member this.ReceiveBasicMessage (m: Message) = async {
+        let sender = {Ip = IPAddress.Parse(m.address); Port = m.port}
+        // Updating the state.
+        this.State <- this.State + m.contents
+        // If current node has already taken a snapshot of its state,
+        // then put received message in the list of messages to snapshot.
+        if this.ShouldTakeASnapshot = AlreadyDone then
+            this.ChannelStatesSnapshot.[sender].Add(m)
+        if m.needAck then
+            do! this.AcknowledgeMessage m.id sender
+    }
+
+
+    (* Acknowledgements *)
+
+    member this.AcknowledgeMessage messageId (endpoint: ServerEndpoint) = async {
+        ()
+    }
+
+    member this.ReceiveAcknowledgement () = async {
+        ()
+    }
+
+    /// Processing of invalid messages.
+    member this.ReceivedInvalidMessage () = async {
+        printfn "Received invalid message type."
+    }
 
     /// Serves a purpose of message processing dispatcher.
     /// Called by ReceiveMessage function, as soon as the full array of message bytes is received.
@@ -33,22 +216,15 @@ type SnapshotNode(id, ipAddress, port) =
         let messageObject = getObjectFromBytes messageBytes
 
         match messageObject with
-        | :? Hello as hello ->
-            do! this.ReceivedHelloMessage hello
-            printfn "Received a hello"
-            let sender = {Ip = IPAddress.Parse(hello.address); Port = hello.port}
-            if hello.needAck then
-                do! this.SendAckTo sender
-            do! this.SendHeyaMessageTo sender
-        | :? Heya  as heya  ->
-            do! this.ReceivedHeyaMessage heya
-            printfn "Received a heya"
-            if heya.needAck then
-                do! this.SendAckTo {Ip = IPAddress.Parse(heya.address); Port = heya.port}
-        | :? Ack as ack ->
-            printfn "Received ACK: %A" ack.ack
+        | :? Message as message ->
+            do! this.ReceiveBasicMessage message
+            printfn "Received a message"
+            //do! this.SendHeyaMessageTo sender
         | _ ->
             do! this.ReceivedInvalidMessage()
             printfn "Received an invalid message"
+
+        ()
     }
-    member this.Start = this.n.Start
+
+    member this.Start = this.N.Start
--- src/DataContracts/DataContracts.fs
+++ src/DataContracts/DataContracts.fs
@@ -19,3 +19,29 @@ type Hello =
 type Ack = {ack: string}
 
 type Marker = {marker: string}
+
+/// Basic message is a type used for communication between nodes.
+/// Each basic message has a unique ID.
+/// Address and port are sender's endpoint for receiving messages.
+type BasicMessage<'a> = {
+    id: string
+    contents: 'a
+    address: string
+    port: int
+    needAck: bool
+}
+
+/// Snapshot message is a marker message to indicate that a node needs to
+/// take a snapshot of its state and state of its incoming channels.
+/// Node who sends a snapshot message also sends a list of
+/// messages it sent to current node and thinks are now still in transit.
+type SnapshotMessage<'a> = {
+    inTransit: BasicMessage<'a> list
+    address: string
+    port: int
+}
+
+/// Acknowledgement is necessary to confirm that a certain message was successfully received by some node
+type Acknowledgement = {
+    messageId: string
+}
